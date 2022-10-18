78a1a1e21d05bc0a27f5a44ca8f74eedce2556bc
Honeybunch <amt3824@g.rit.edu>
2022-10-18 14:41:45 -0700

    Updating basisu port

--- ports/basisu/portfile.cmake
+++ ports/basisu/portfile.cmake
@@ -1,25 +1,22 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO jherico/basis_universal
-    REF 497875f756ed0e3eb62e0ff08d55c62242f4be74
-    SHA512 2293b78620a7ed510dbecf48bcae5f4b8524fe9020f864c8e79cf94ea9d95d51dddf83a5b4ea29cc95db19f87137bfef1cb68b7fbc6387e08bb42898d81c9303
+    REPO Honeybunch/basis_universal
+    REF 22dd77180c0568e1c5e1b11b21fe296e5c0c2696
+    SHA512 d05a20c7baedbf5e6430ff148d5340dbf01385bbfc28d14e17fe0023bb04b407df16ee0c2ac4c96252e5d3094c7befa1a5673d332353fd21e63fcec186dc50e6
     HEAD_REF master
-    PATCHES fix-addostream.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
 
-vcpkg_copy_pdbs()
-
-vcpkg_copy_tools(TOOL_NAMES "basisu_tool" AUTO_CLEAN)
+vcpkg_copy_tools(TOOL_NAMES "basisu" AUTO_CLEAN)
 
 # Remove unnecessary files
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
